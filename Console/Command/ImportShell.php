a38d8bd9c27c51c2de748ec85207195588efa39b
YuichiNoami <avocadohiroshi@gmail.com>
2019-05-09 06:38:35 +0900

    不要なコメントを削除

--- Console/Command/ImportShell.php
+++ Console/Command/ImportShell.php
@@ -1,6 +1,6 @@
 <?php
 class ImportShell extends AppShell {
-    public $tasks = array('ImportEvent'); // Console/Command/Task/SoundTask.php に作成
+    public $tasks = array('ImportEvent');
     public function main() {
         $this->ImportEvent->execute();
     }
